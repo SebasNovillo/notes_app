ae4c51aac515a768335e4aa5d78891ef4f72f6ec
SebasNovillo <sebasnovillo66@gmail.com>
2025-08-19 17:46:27 -0500

    LogIn UI

---
+++ frontend/src/App.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from './pages/Home/Home'
+import LogIn from './pages/LogIn/LogIn';
+import SignUp from './pages/SignUp/SignUp';
+
+const routes = (
+  <Router>
+    <Routes>
+      <Route path='/dashboard' exact element={<Home />} />
+      <Route path='/login' exact element={<LogIn />} />
+      <Route path='/signup' exact element={<SignUp />} />
+    </Routes>
+  </Router>
+);
+
+const App = () => {
+  return (
+    <>
+      {/* <Home /> */}
+      {routes}
+    </>
+  )
+}
+
+export default App
